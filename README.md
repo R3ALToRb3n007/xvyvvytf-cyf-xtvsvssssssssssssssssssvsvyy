33d7c18949c5b9cdce539d5f4db4b3b48c4c69fb
R3ALToRb3n007 <57048075+R3ALToRb3n007@users.noreply.github.com>
2019-10-26 22:32:45 +0200

    xvyvvytf-cyf-xtvsvssssssssssssssssssvsvyy
    xvyvvytf-cyf-xtvsvssssssssssssssssssvsvyy

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# xvyvvytf-cyf-xtvsvssssssssssssssssssvsvyy
-xvyvvytf cyf xtvsvssssssssssssssssssvsvyy not copy pls
+# Hentaibot
+Ein Discord.js-Chatbot, geschrieben in JavaScript.
+
+Installation:
+
+1.
+Downloade dir den Bot und entpacke die .zip-Datei.
+
+2.
+Füge die Discord.js-API dem Bot hinzu.
+
+3.
+Installiere Node.js.
+
+4.
+Schreib deinen Login-Token in die dafür vorgesehene Zeile.
+
+5.
+Öffne den Ordner, in welchem sich der Bot befindet mit der Kommandozeile und schreibe in die Kommandozeile "node .".
---
+++ index.js
@@ -0,0 +1,145 @@
+const Discord = require('discord.js');
+const hbot = new Discord.Client();
+
+
+//Hilfe 
+hbot.on('message', (message) => {
+    if(message.content == '/help') {
+        message.channel.sendMessage('Anime Bot: Info - Bitte nicht in caps schreiben!: :zap:Befehle:\n\n:zap: /bilder -> Sendet ein Animebild\n:zap:/gif -> Sendet ein Animegif\n:zap:/hentai -> Schreibt einen Hentainamen');
+    }
+});
+
+
+//Generiert bei dem Befehl random Bilder
+hbot.on('message', (message) => {
+    if(message.content == '/anime'){
+    var randomPic = Math.floor((Math.random()*12)+1);
+
+    if(randomPic == 1)
+    message.channel.sendFile('./images/anime.jpg');
+
+    if(randomPic == 2)
+    message.channel.sendFile('./images/anime2.jpg');
+
+    if(randomPic == 3)
+    message.channel.sendFile('./images/anime3.jpg');
+
+    if(randomPic == 4)
+    message.channel.sendFile('./images/anime4.jpg');
+
+    if(randomPic == 5)
+    message.channel.sendFile('./images/anime5.jpg')
+
+    if(randomPic == 6)
+    message.channel.sendFile('./images/anime6.jpg')
+
+    if(randomPic == 7)
+    message.channel.sendFile('./images/anime7.jpg')
+
+    if(randomPic == 8)
+    message.channel.sendFile('./images/anime8.jpg')
+
+    if(randomPic == 9)
+    message.channel.sendFile('./images/anime9.jpg')
+
+    if(randomPic == 10)
+    message.channel.sendFile('./images/anime10.jpg')
+
+    if(randomPic == 11)
+    message.channel.sendFile('./images/anime11.jpg')
+
+    if(randomPic == 14)
+    message.channel.sendFile('./images/anime12.png')
+
+    if(randomPic == 16)
+    message.channel.sendFile('./images/anime12.png')
+
+    if(randomPic == 17)
+    message.channel.sendFile('./images/anime12.png')
+
+    if(randomPic == 18)
+    message.channel.sendFile('./images/anime12.png')
+
+    if(randomPic == 19)
+    message.channel.sendFile('./images/anime12.png')
+
+    if(randomPic == 20)
+    message.channel.sendFile('./images/anime12.png')
+
+    if(randomPic == 21)
+    message.channel.sendFile('./images/anime12.png')
+    }
+});
+    
+//Generiert bei dem Befehl random GIFs
+hbot.on('message', (message) => {
+    if(message.content == '/gif'){
+    var randomGIF = Math.floor((Math.random()*3)+1);
+
+    if(randomGIF == 1)
+    message.channel.sendFile('./gifs/gif1.gif');
+
+    if(randomGIF == 2)
+    message.channel.sendFile('./gifs/gif2.gif');
+
+    if(randomGIF == 3)
+    message.channel.sendFile('./gifs/gif3.gif');
+    }
+});
+
+hbot.on('message', (message) => {
+    if(message.content == '/hentai'){
+    var randomHentai = Math.floor((Math.random()*16)+1);
+
+    if(randomHentai == 1)
+    message.channel.sendMessage('Saimin Class - Episode 1');
+
+    if(randomHentai == 2)
+    message.channel.sendMessage('Hataraku Otona No Renai Jijou - Episode 1');
+
+    if(randomHentai == 3)
+    message.channel.sendMessage('Mashou No Nie - Episode 2');
+
+    if(randomHentai == 4)
+    message.channel.sendMessage('Menkui! - Episode 2');
+
+    if(randomHentai == 5)
+    message.channel.sendMessage('Menkui! - Episode 1');
+
+    if(randomHentai == 6)
+    message.channel.sendMessage('Shoujo-Tachi No Sadism The Animation - Episode 2');
+
+    if(randomHentai == 7)
+    message.channel.sendMessage('Ima Kara Atashi... - Episode 2');
+
+    if(randomHentai == 8)
+    message.channel.sendMessage('Kimi No Mana Wa Rina Witch - Episode 1');
+
+    if(randomHentai == 9)
+    message.channel.sendMessage('Bokura No Sex - Episode 1');
+
+    if(randomHentai == 10)
+    message.channel.sendMessage('Shiiku X Kanojo - Episode 3');
+
+    if(randomHentai == 11)
+    message.channel.sendMessage('Futabu! - Episode 2');
+
+    if(randomHentai == 12)
+    message.channel.sendMessage('Futabu! - Episode 1');
+
+    if(randomHentai == 13)
+    message.channel.sendMessage('Pretty X Cation 2 - Episode 1');
+
+    if(randomHentai == 14)
+    message.channel.sendMessage('Hachishaku Hachiwa Keraku Meguri: Igyou');
+
+    if(randomHentai == 15)
+    message.channel.sendMessage('JK To Ero Konbini Tenchou - Episode 3');
+
+    if(randomHentai == 16)
+    message.channel.sendMessage('Bangable Girl - Episode 2');
+    }
+});
+
+//Login-Token hier einfügen
+hbot.login('NjM3NzQyNjM1MDQ1NjE3Njc0.XbSnFA.FEdFDCUT3myk5FtRhuAorM_CEqg');
